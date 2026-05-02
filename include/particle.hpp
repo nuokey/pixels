94f2b7e314ee6d516922c11e3f1fc9c28424bfc0
Stepan-Stryapko <2017.despicable.me@gmail.com>
2026-05-02 11:28:42 +0300

    -No this anymore

--- include/particle.hpp
+++ include/particle.hpp
@@ -13,6 +13,6 @@ class Particle {
     sf::Color color;
     sf::RectangleShape rect;
     
-    Particle(float x, float y, float vx, float vy, sf::Color color, int ttl);
+    Particle(float x_, float y_, float vx_, float vy_, sf::Color color_, int ttl_);
     void update(float dt, Camera camera);
 };
