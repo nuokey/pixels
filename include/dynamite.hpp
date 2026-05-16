7113cac9138d86447e33c11e88d08be3ecf741e1
Stepan-Stryapko <2017.despicable.me@gmail.com>
2026-05-16 09:32:36 +0300

    class dynamite changed

--- include/dynamite.hpp
+++ include/dynamite.hpp
@@ -1,16 +1,12 @@
 #pragma once
 
-#include "projectile.hpp"
+#include "pixel.hpp"
 #include <vector>
 
-class Dynamite : public Projectile {
-private:
-    static const int explosionCount = 12;      
-    static const float explosionSpeed;         
-    float explosionDamage;                     
+class Projectile;
 
+class Dynamite : public Pixel {
 public:
-    Dynamite(float x_, float y_, float vx_, float vy_, sf::Color color_, float damage_);
-    
-    void hit(Pixel* pixel, std::vector<Projectile>* projectiles, int index) override;
+    Dynamite(float x, float y);
+    void explode(std::vector<Projectile>* projectiles);
 };
